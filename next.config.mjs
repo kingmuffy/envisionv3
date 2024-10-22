1c91d0c5fc7ad2029e3b7967228aa009faf7277e
kingmuffy <kingmuffyatse@gmail.com>
2024-10-22 09:42:33 +0100

    f

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  api: {
-    bodyParser: {
-      sizeLimit: "20mb",
-    },
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
